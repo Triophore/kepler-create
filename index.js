5a1e857457c33456c3b2c2d0bc0bf91368293a7b
venusdharan <venusdharan@gmail.com>
2021-05-19 03:23:51 +0530

    typo fix

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 #!/usr/bin/env node
 console.log("Awesome Command");
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
-console.log(process.cwd())
+console.log(process.cwd());
