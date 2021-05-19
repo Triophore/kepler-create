6ca4c882f77787bbf63e79f2d41b1358099c84dc
venusdharan <venusdharan@gmail.com>
2021-05-20 01:11:45 +0530

    typo fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const path = require('path');
 const generator = path.resolve(__dirname, './')
-console.log("Awesome Command");
-console.log(process);
+
+console.log(process.env);
 
